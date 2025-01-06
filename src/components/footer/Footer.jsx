b698d5fe0c5f6449390e12f9e051fd4e79b8b971
arman2766 <arman2766@gmail.com>
2025-01-06 22:03:32 +0530

    feat: Footer page add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,49 @@
-
+import { Facebook, Instagram, Twitter, Youtube } from "lucide-react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/svg/aCart-Logo.svg";
+import "./footer.scss";
 
 export const Footer = () => {
+  const footerList = (
+    <ul className="footer-list-item">
+      <li>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about-us"}>About Us</Link>
+        <Link to={"/shop"}>Shop</Link>
+        <Link to={"/men"}>Men</Link>
+        <Link to={"/women"}>Women</Link>
+        <Link to={"/contact"}>Contact Us</Link>
+      </li>
+    </ul>
+  );
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="footer-container">
+        <div className="logo">
+          <Link to={"/"}>
+            {" "}
+            <img src={Logo} alt="" />
+          </Link>
+        </div>
+        <div className="items">{footerList}</div>
+        <div className="social-media">
+          <a href="#">
+            <Facebook />
+          </a>
+          <a href="#">
+            <Twitter />
+          </a>
+          <a href="#">
+            <Instagram />
+          </a>
+          <a href="#">
+            <Youtube />
+          </a>
+        </div>
+        <div className="copyright">
+          <p>&copy; 2022 aCart. All rights reserved.</p>
+        </div>
+      </div>
+    </>
+  );
+};
